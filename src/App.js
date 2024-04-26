d0a762d3d5ae3cb438c4894143f95eef9d114b48
Rekg <106755026+Rekg@users.noreply.github.com>
2024-04-26 13:56:36 -0700

    weathercss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import WeatherApp from "./Components/WeatherApp/WeatherApp";
-
+import search_icon from "../src/Components/Assets/search.png";
 function App() {
   return (
     <div className="App">
